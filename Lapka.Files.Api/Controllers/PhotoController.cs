b7dc02734e4ef7ac3d6e2b582309a7a84656c9c6
Quazino <wardls33@gmail.com>
2021-09-23 18:40:13 +0200

    Swagger info about getting photo.

--- Lapka.Files.Api/Controllers/PhotoController.cs
+++ Lapka.Files.Api/Controllers/PhotoController.cs
@@ -6,6 +6,7 @@ using Lapka.Files.Application.Dto;
 using Lapka.Files.Application.Queries;
 using Lapka.Files.Core.ValueObjects;
 using Lapka.Files.Infrastructure;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Lapka.Files.Api.Controllers
@@ -21,6 +22,13 @@ namespace Lapka.Files.Api.Controllers
             _queryDispatcher = queryDispatcher;
         }
         
+        /// <summary>
+        /// Gets photo by path.
+        /// </summary>
+        /// <returns>Photo</returns>
+        /// <response code="200">If successfully got photo, returns photo as image</response>
+        /// <response code="404">If photo was not found</response>
+        [ProducesResponseType(typeof(FileContentResult), StatusCodes.Status200OK)]
         [HttpGet("{photoPath}")]
         public async Task<IActionResult> Get(string photoPath, BucketName bucketName)
         {
